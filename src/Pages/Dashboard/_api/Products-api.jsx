6b86bb40c11575c4f175b2692a9276d36b322fa0
pourya-asrzad <poriaasrzad@gmail.com>
2024-10-02 14:43:41 +0330

    refactor : change limitation number

--- src/Pages/Dashboard/_api/Products-api.jsx
+++ src/Pages/Dashboard/_api/Products-api.jsx
@@ -1,6 +1,6 @@
 import httpRequest from "../../../Services/http-request";
 
-export const getProducts = async (page = 1, itemsPerPage = 2, category) => {
+export const getProducts = async (page = 1, itemsPerPage = 10, category) => {
   const url = `/api/products?page=${page}&limit=${itemsPerPage}${
     category && `&category=${category}`
   }`;
